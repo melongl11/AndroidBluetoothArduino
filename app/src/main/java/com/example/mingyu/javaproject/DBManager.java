97cf09116e0ce2261aa46b97df4c2e252867d63c
지민규 <melongl11@naver.com>
2017-05-22 23:40:25 +0900

    Bluetooth, DB Test

---
+++ app/src/main/java/com/example/mingyu/javaproject/DBManager.java
@@ -0,0 +1,63 @@
+package com.example.mingyu.javaproject;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+
+
+public class DBManager extends SQLiteOpenHelper{
+
+    //DB생성
+    public DBManager(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE FOOD_LIST( _id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, price INTEGER);");
+    }
+
+    @Override
+    public  void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    public void insert(String _query) {
+        SQLiteDatabase db = getWritableDatabase();
+        db.execSQL(_query);
+        db.close();
+    }
+
+    public void update(String _query) {
+        SQLiteDatabase db = getWritableDatabase();
+        db.execSQL(_query);
+        db.close();
+    }
+
+    public void delete(String _query) {
+        SQLiteDatabase db = getWritableDatabase();
+        db.execSQL(_query);
+        db.close();
+    }
+
+    public String PrintData() {
+        SQLiteDatabase db = getReadableDatabase();
+        String str = "";
+
+        Cursor cursor = db.rawQuery("select * from FOOD_LIST", null);
+        while(cursor.moveToNext()) {
+            str += cursor.getInt(0)
+                    + " : foodName "
+                    + cursor.getString(1)
+                    + ", price = "
+                    + cursor.getInt(2)
+                    +"\n";
+        }
+        return str;
+    }
+
+
+}
--- app/src/main/java/com/example/mingyu/javaproject/MainActivity.java
+++ app/src/main/java/com/example/mingyu/javaproject/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.mingyu.javaproject;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,66 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final DBManager dbManager = new DBManager(getApplicationContext(), "Food.db", null, 1);
+
+        final EditText etName = (EditText) findViewById(R.id.et_foodname);
+        final EditText etPrice = (EditText) findViewById(R.id.et_price);
+
+        final TextView tvResult = (TextView) findViewById(R.id.tv_result);
+
+        Button btnInsert = (Button) findViewById(R.id.btn_insert);
+        btnInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = etName.getText().toString();
+                String price = etPrice.getText().toString();
+                dbManager.insert("insert into FOOD_LIST values(null, '" + name + "', "+ price + ");");
+                tvResult.setText( dbManager.PrintData());
+            }
+        });
+
+        Button btnUpdate = (Button) findViewById(R.id.btn_update);
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = etName.getText().toString();
+                String price = etPrice.getText().toString();
+                dbManager.update("update FOOD_LIST set price = " + price + " where name = '" + name + "';");
+
+                tvResult.setText(dbManager.PrintData());
+            }
+
+        });
+
+        Button btnDelete = (Button) findViewById(R.id.btn_delete);
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = etName.getText().toString();
+                dbManager.delete("delete from FOOD_LIST where name = '" + name + "';");
+
+                tvResult.setText(dbManager.PrintData());
+            }
+        });
+
+        Button btnSelect = (Button) findViewById(R.id.btn_select);
+        btnSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tvResult.setText(dbManager.PrintData());
+            }
+        });
+
+        Button btnBluetooth = (Button) findViewById(R.id.btn_bluetooth);
+        btnBluetooth.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                if(view.getId() == R.id.btn_bluetooth) {
+                    Intent intent = new Intent(MainActivity.this, BTManager.class);
+                    startActivity(intent);
+                }
+            }
+        });
     }
 }
